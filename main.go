a9183343a5c68cac882824d77753121915bc5648
Vincent Gabb <vincentgabb@gmail.com>
2020-03-26 23:57:31 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+// Programmer name: Vincent Gabb  
+// Date completed:  3/26/2020
+// Description: ASCII ART
+
+package main
+
+import "fmt"
+
+//create a function that draws ASCII art of some type
+func Art(){
+  fmt.Println(" _   _ _____ _     _     ___  ")
+  fmt.Println("| | | | ____| |   | |   | _ | ")
+  fmt.Println("| | | |  _| | |   | |   || ||")
+  fmt.Println("| |_| |  _| | |   | |   || ||")
+  fmt.Println("|  _  | |___| |___| |__ ||_||")
+  fmt.Println("|_| |_|_____|_____|_____|___| ")
+
+}
+
+func main() {
+    //call your ascii art function
+    Art()
+}
